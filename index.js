37f0e56ae8f9fdd324ea15f25db9b94237e3cebb
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-03-01 14:32:41 +0400

    Fixed bug

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let key = "d45ecf04";
 
 let getMovie = () => {
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+    let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
 
     if (movieName.length <= 0) {
         result.innerHTML = `<h3 class="msg">Please enter a movie name </h3>`;
